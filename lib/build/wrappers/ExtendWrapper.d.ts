dd2edbafe1eb047c2ee6d51b60a1ab5fbd5d91a4
Mojtaba Abaie Shoushtary <mojtaba.abaie@gmail.com>
2020-05-20 03:03:41 +0430

    after  build

---
+++ lib/build/wrappers/ExtendWrapper.d.ts
@@ -0,0 +1,7 @@
+import { Omit } from '@material-ui/core';
+import { InlineWrapperProps } from './InlineWrapper';
+import { ModalWrapperProps } from './ModalWrapper';
+export declare type ExtendWrapper<T extends InlineWrapperProps | ModalWrapperProps> = Omit<
+  T,
+  'onChange' | 'value' | 'format'
+>;
